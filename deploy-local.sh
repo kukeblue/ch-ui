596b67e5ae5f6b7b3cf3eae65d11ba4ffcdcd6d6
kukechen <chen.huan@infoloop.cn>
2021-08-23 08:07:08 +0800

    ~

---
+++ deploy-local.sh
@@ -0,0 +1,3 @@
+rm -rf ./dist;
+yarn build;
+cp -r /Users/huanchen/Project/ch-ui/dist /Users/huanchen/Project/out/coin-factory-frontend/node_modules/ch-ui/dist;
--- src/component/ChForm/index.tsx
+++ src/component/ChForm/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {
-    Form,
-    Input,
-    Button,
-    Checkbox,
-    Radio,
-    Select,
-    Upload,
-    Row,
-    Col,
-    Cascader,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Radio,
+  Select,
+  Upload,
+  Row,
+  Col,
+  Cascader,
 } from 'antd';
 import { FormInstance } from 'antd/lib/form/hooks/useForm';
 import { UploadOutlined } from '@ant-design/icons';
@@ -18,193 +18,196 @@ import regionOptions from '../../chutils/regionOptions';
 import './index.less';
 const { Option } = Select;
 export enum FormItemType {
-    input = 'input',
-    radioGroup = 'radio-group',
-    select = 'select',
-    upload = 'upload',
-    multipleSelect = 'mutipleSelect',
-    regionSelect = 'region-select',
+  input = 'input',
+  radioGroup = 'radio-group',
+  select = 'select',
+  upload = 'upload',
+  multipleSelect = 'mutipleSelect',
+  regionSelect = 'region-select',
 }
 interface FormItemRule {
-    validator?: (rule: any, value: any, callback: (v: any) => void) => void;
-    required: boolean;
-    message: string;
+  validator?: (rule: any, value: any, callback: (v: any) => void) => void;
+  required: boolean;
+  message: string;
 }
 type CheckboxValueType = string | number;
 interface FormItemOptionsType {
-    label: React.ReactNode;
-    value: CheckboxValueType;
-    style?: React.CSSProperties;
-    disabled?: boolean;
+  label: React.ReactNode;
+  value: CheckboxValueType;
+  style?: React.CSSProperties;
+  disabled?: boolean;
 }
 export interface FormDataItem {
-    key?: string;
-    placeholder?: string;
-    itemshow?: (editor?: any, form?: FormInstance<any>) => boolean;
-    type: FormItemType;
-    label: string;
-    name: string;
-    rules?: FormItemRule[];
-    options?: FormItemOptionsType[];
-    initialValue?: any;
-    valuePropName?: string;
-    getValueFromEvent?: (e: any) => any;
-    uploadurl?: string;
-    uploadType?: 'picture' | 'text' | 'picture-card' | undefined;
-    uploadname?: string;
-    uploadheader?: any;
-    layout?: {
-        span?: number;
-        offset?: number;
-    };
+  key?: string;
+  placeholder?: string;
+  itemshow?: (editor?: any, form?: FormInstance<any>) => boolean;
+  type: FormItemType;
+  inputtype?: string;
+  label: string;
+  name: string;
+  rules?: FormItemRule[];
+  options?: FormItemOptionsType[];
+  initialValue?: any;
+  valuePropName?: string;
+  getValueFromEvent?: (e: any) => any;
+  uploadurl?: string;
+  uploadType?: 'picture' | 'text' | 'picture-card' | undefined;
+  uploadname?: string;
+  uploadheader?: any;
+  layout?: {
+    span?: number;
+    offset?: number;
+  };
 }
 interface ChFormProps {
-    showclear?: boolean;
-    submitname?: string;
-    formData: FormDataItem[];
-    onFinish?: (values: any) => void;
-    form?: FormInstance<any>;
-    editor?: any;
-    layout?: {
-        labelCol?: { span?: number; offset?: number };
-        wrapperCol?: { span?: number; offset?: number };
-    };
+  className?: string;
+  showclear?: boolean;
+  submitname?: string;
+  formData: FormDataItem[];
+  onFinish?: (values: any) => void;
+  form?: FormInstance<any>;
+  editor?: any;
+  layout?: {
+    labelCol?: { span?: number; offset?: number };
+    wrapperCol?: { span?: number; offset?: number };
+  };
 }
 
 export default ({
-    formData,
-    onFinish,
-    form,
-    editor,
-    layout,
-    submitname,
-    showclear,
+  formData,
+  onFinish,
+  form,
+  editor,
+  layout,
+  submitname,
+  showclear,
+  className,
 }: ChFormProps) => {
-    if (!form) {
-        [form] = useForm();
+  if (!form) {
+    [form] = useForm();
+  }
+
+  const _layout = layout || {
+    labelCol: { span: 24 },
+    wrapperCol: { span: 24 },
+  };
+  // @type JSX Function | @dec 渲染单个formItem
+  const renderFormItem = (item: FormDataItem) => {
+    let dom;
+    switch (item.type) {
+      case 'input':
+        dom = <Input type={item.inputtype} placeholder={item.placeholder} />;
+        break;
+      case 'radio-group':
+        dom = (
+          <Radio.Group
+            options={item.options}
+            optionType="button"
+            buttonStyle="solid"
+          />
+        );
+        break;
+      case 'select':
+        dom = (
+          <Select placeholder={item.placeholder}>
+            {item.options?.map((item, index) => {
+              return (
+                <Option key={index} value={item.value}>
+                  {item.label}
+                </Option>
+              );
+            })}
+          </Select>
+        );
+        break;
+      case 'upload':
+        dom = (
+          <Upload
+            headers={
+              item.uploadheader
+              // { Auth: getObCache('user') && getObCache('user').token}
+            }
+            multiple
+            name={item.uploadname || 'file'}
+            action={item.uploadurl ? item.uploadurl : '/fileUpload'}
+            listType={item.uploadType}
+          >
+            <Button icon={<UploadOutlined />}>点击文件</Button>
+          </Upload>
+        );
+        break;
+      case 'mutipleSelect':
+        dom = (
+          <Select placeholder={item.placeholder} mode="multiple">
+            {item.options?.map((item, index) => {
+              return (
+                <Option key={'_' + index} value={item.value}>
+                  {item.label}
+                </Option>
+              );
+            })}
+          </Select>
+        );
+        break;
+      case 'region-select':
+        dom = (
+          <Cascader
+            options={regionOptions}
+            placeholder={item.placeholder || '请选择地区'}
+          />
+        );
+        break;
+      default:
+        dom = <Input placeholder={item.placeholder} />;
     }
+    return dom;
+  };
+  const buildFormItemProps = (item: FormDataItem) => {
+    item.key = item.key || `formData_${item.name}`;
+    const { inputtype, itemshow, ...ob } = item;
+    return ob;
+  };
 
-    const _layout = layout || {
-        labelCol: { span: 24 },
-        wrapperCol: { span: 24 },
-    };
-    // @type JSX Function | @dec 渲染单个formItem
-    const renderFormItem = (item: FormDataItem) => {
-        let dom;
-        switch (item.type) {
-            case 'input':
-                dom = <Input placeholder={item.placeholder} />;
-                break;
-            case 'radio-group':
-                dom = (
-                    <Radio.Group
-                        options={item.options}
-                        optionType="button"
-                        buttonStyle="solid"
-                    />
-                );
-                break;
-            case 'select':
-                dom = (
-                    <Select placeholder={item.placeholder}>
-                        {item.options?.map((item, index) => {
-                            return (
-                                <Option key={index} value={item.value}>
-                                    {item.label}
-                                </Option>
-                            );
-                        })}
-                    </Select>
-                );
-                break;
-            case 'upload':
-                dom = (
-                    <Upload
-                        headers={
-                            item.uploadheader
-                            // { Auth: getObCache('user') && getObCache('user').token}
-                        }
-                        multiple
-                        name={item.uploadname || 'file'}
-                        action={item.uploadurl ? item.uploadurl : '/fileUpload'}
-                        listType={item.uploadType}
-                    >
-                        <Button icon={<UploadOutlined />}>点击文件</Button>
-                    </Upload>
-                );
-                break;
-            case 'mutipleSelect':
-                dom = (
-                    <Select placeholder={item.placeholder} mode="multiple">
-                        {item.options?.map((item, index) => {
-                            return (
-                                <Option key={'_' + index} value={item.value}>
-                                    {item.label}
-                                </Option>
-                            );
-                        })}
-                    </Select>
-                );
-                break;
-            case 'region-select':
-                dom = (
-                    <Cascader
-                        options={regionOptions}
-                        placeholder={item.placeholder || '请选择地区'}
-                    />
-                );
-                break;
-            default:
-                dom = <Input placeholder={item.placeholder} />;
-        }
-        return dom;
-    };
-    const buildFormItemProps = (item: FormDataItem) => {
-        item.key = item.key || `formData_${item.name}`;
-        const { itemshow, ...ob } = item;
-        return ob;
-    };
-
-    return (
-        <div>
-            <Form preserve={false} form={form} {..._layout} onFinish={onFinish}>
-                <Row>
-                    {formData &&
-                        formData.map((item, index) => {
-                            let layout = item.layout || { span: 24 };
-                            let formItemProps = buildFormItemProps(item);
-                            if (item.itemshow && !item.itemshow(editor, form)) {
-                                return null;
-                            }
-                            return (
-                                <Col key={formItemProps.key || index} {...layout}>
-                                    <Form.Item {...formItemProps}>
-                                        {renderFormItem(item)}
-                                    </Form.Item>
-                                </Col>
-                            );
-                        })}
-                    <Col offset={1} span={4}>
-                        {onFinish && (
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit">
-                                    {submitname || '确定'}
-                                </Button>
-                                {showclear && (
-                                    <Button
-                                        onClick={() => {
-                                            form?.resetFields();
-                                        }}
-                                        style={{ marginLeft: 10 }}
-                                    >
-                                        清空选项
-                                    </Button>
-                                )}
-                            </Form.Item>
-                        )}
-                    </Col>
-                </Row>
-            </Form>
-        </div>
-    );
+  return (
+    <div className={className}>
+      <Form preserve={false} form={form} {..._layout} onFinish={onFinish}>
+        <Row>
+          {formData &&
+            formData.map((item, index) => {
+              let layout = item.layout || { span: 24 };
+              let formItemProps = buildFormItemProps(item);
+              if (item.itemshow && !item.itemshow(editor, form)) {
+                return null;
+              }
+              return (
+                <Col key={formItemProps.key || index} {...layout}>
+                  <Form.Item {...formItemProps}>
+                    {renderFormItem(item)}
+                  </Form.Item>
+                </Col>
+              );
+            })}
+          <Col offset={1} span={4}>
+            {onFinish && (
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  {submitname || '确定'}
+                </Button>
+                {showclear && (
+                  <Button
+                    onClick={() => {
+                      form?.resetFields();
+                    }}
+                    style={{ marginLeft: 10 }}
+                  >
+                    清空选项
+                  </Button>
+                )}
+              </Form.Item>
+            )}
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  );
 };
